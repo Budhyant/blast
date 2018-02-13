4c3e50ca0ce736f859d676d5faab7f5e098db014
Johnny <johnny@jober.io>
2018-02-13 22:43:03 +0900

    p_s is evaluated

--- test.py
+++ test.py
@@ -40,7 +40,6 @@ while True:
 TNT_EQ_WT = neQty * TNT_EQ_Fig
 scDist = soDist / math.pow(TNT_EQ_WT, 0.3333)
 logScaledDist = math.log(scDist, 10)
-print(logScaledDist)
 
 # ----------------------------------------------------
 # get Ps
@@ -56,53 +55,84 @@ def get_list_fn_u(u, list_slop_u):
         storage.append(get_fn_u_pow(u, item, idx + 1))
     return storage
 
+ps_u = -0.214362789151 + 1.35034249993 * logScaledDist
+# print(ps_u)
 
-list_slope_ps_u = [
-    1.69012801396,
+list_slope_ps_u_air = [
+    -1.69012801396,
     0.00804973591951,
     0.336743114941,
-    0.00516226351334,
-    0.0809228619888,
-    0.00478507266747,
+    -0.00516226351334,
+    -0.0809228619888,
+    -0.00478507266747,
     0.00793030472242,
     0.0007684469735
 ]
 
-ps_u = -0.214362789151 + 1.35034249993 * logScaledDist
-print(ps_u)
+list_slope_ps_u_surface = [
+    -1.6958988741,
+    -0.154159376846,
+    0.514060730593,
+    0.0988534365274,
+    -0.293912623038,
+    -0.0268112345019,
+    0.109097496421,
+    0.00162846756311,
+    - 0.0214631030242,
+    0.0001456723382,
+    0.00167847752266,
+]
 
-list_fn_ps_u = get_list_fn_u(ps_u, list_slope_ps_u)
-print(list_fn_ps_u)
+list_slope_ps_u = [
+    list_slope_ps_u_air,
+    list_slope_ps_u_surface
+]
 
-ps_y = 2.611368669 - list_fn_ps_u[0] + list_fn_ps_u[1] + list_fn_ps_u[2] - list_fn_ps_u[3] - list_fn_ps_u[4] - list_fn_ps_u[5] + list_fn_ps_u[6] + list_fn_ps_u[7]
-print(ps_y)
+arr_const_y_p_s = [
+    2.611368669,
+    2.78076916577
+]
 
-appl_lower_rng_filter = None
-appl_upper_rng_filter = None
+for idx, slopes in enumerate(list_slope_ps_u):
+    list_fn_ps_u = get_list_fn_u(ps_u, slopes)
+    # print('idx: ', idx, 'list_fn_ps_u', list_fn_ps_u)
 
-if scDist < 0.0531:
-    appl_lower_rng_filter = 0
-else:
-    appl_lower_rng_filter = ps_y
-print(appl_lower_rng_filter)
+    ps_y = arr_const_y_p_s[idx] + sum(list_fn_ps_u)
+    # print('ps_y', ps_y)
 
-if scDist > 40:
-    appl_upper_rng_filter = 0
-else:
-    appl_upper_rng_filter = ps_y
-ps_checksum = appl_lower_rng_filter + appl_upper_rng_filter
+    appl_lower_rng_filter = None
+    appl_upper_rng_filter = None
 
-filtered_result = None
-if ps_checksum == 2 * ps_y:
-    filtered_result = ps_y
-else:
-    filtered_result = 0
+    if scDist < 0.0531:
+        appl_lower_rng_filter = 0
+    else:
+        appl_lower_rng_filter = ps_y
+    # print(appl_lower_rng_filter)
 
-ps_anti_log_y = None
-if filtered_result == 0:
-    ps_anti_log_y = 0
-else:
-    ps_anti_log_y = 10**ps_y
+    if scDist > 40:
+        appl_upper_rng_filter = 0
+    else:
+        appl_upper_rng_filter = ps_y
+    ps_checksum = appl_lower_rng_filter + appl_upper_rng_filter
 
-print(ps_anti_log_y)
-print('none: ',None)
+    filtered_result = None
+    if ps_checksum == 2 * ps_y:
+        filtered_result = ps_y
+    else:
+        filtered_result = 0
+
+    ps_anti_log_y = None
+    if filtered_result == 0:
+        ps_anti_log_y = 0
+    else:
+        ps_anti_log_y = 10 ** ps_y
+
+    # print(ps_anti_log_y)
+
+    if filtered_result == 0:
+        raise SystemExit("filtered result = 0")
+    else:
+        p_s = ps_anti_log_y
+
+    print('Ps: ', p_s)
+
