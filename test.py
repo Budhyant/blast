d094420d6c72ef00fb157996039a0c8d6ad7fd59
Johnny <johnny@jober.io>
2018-02-13 18:02:07 +0900

    convert code to python 3 grammar

--- test.py
+++ test.py
@@ -10,32 +10,35 @@ TNT_EQ_FIGURE = 1
 
 TNT_EQ_Fig = None
 while True:
-    TNT_EQ_FigInput = input ("select explosive type:\n1. TNT\n2.RDX (Cyclonite)\n3. HMX \n4. Nitroglycerin (liquid)\n5. Compund B (60% RDX 40% TNT)\n6. Semtex\n7. 60% Nitroglycerin dynamite\n\nyour selected type: ")
-    if TNT_EQ_FigInput == 1:
+    TNT_EQ_FigInput = input("select explosive type:\n1. TNT\n2.RDX (Cyclonite)\n3. HMX \n4. Nitroglycerin (liquid)\n5. Compund B (60% RDX 40% TNT)\n6. Semtex\n7. 60% Nitroglycerin dynamite\n\nyour selected type: ")
+    if TNT_EQ_FigInput == '1':
         TNT_EQ_Fig = 1
         break
-    elif TNT_EQ_FigInput == 2:
+    elif TNT_EQ_FigInput == '2':
         TNT_EQ_Fig = 1.185
         break
-    elif TNT_EQ_FigInput == 3:
+    elif TNT_EQ_FigInput == '3':
         TNT_EQ_Fig = 1.256
         break
-    elif TNT_EQ_FigInput == 4:
+    elif TNT_EQ_FigInput == '4':
         TNT_EQ_Fig = 1.481
         break
-    elif TNT_EQ_FigInput == 5:
+    elif TNT_EQ_FigInput == '5':
         TNT_EQ_Fig = 1.148
         break
-    elif TNT_EQ_FigInput == 6:
+    elif TNT_EQ_FigInput == '6':
         TNT_EQ_Fig = 1.25
         break
-    elif TNT_EQ_FigInput == 7:
+    elif TNT_EQ_FigInput == '7':
         TNT_EQ_Fig = 0.6
         break
+    else:
+        raise SystemExit("you should select from the options")
+
 TNT_EQ_WT = neQty * TNT_EQ_Fig
 scDist = soDist / math.pow(TNT_EQ_WT, 0.3333)
 logScaledDist = math.log(scDist, 10)
-print logScaledDist
+print(logScaledDist)
 
 # ----------------------------------------------------
 # get Ps
@@ -61,13 +64,13 @@ list_slope_ps_u = [
 ]
 
 ps_u = -0.214362789151 + 1.35034249993 * logScaledDist
-print ps_u
+print(ps_u)
 
 list_fn_ps_u = get_list_fn_u(ps_u, list_slope_ps_u)
-print list_fn_ps_u
+print(list_fn_ps_u)
 
 ps_y = 2.611368669 - list_fn_ps_u[0] + list_fn_ps_u[1] + list_fn_ps_u[2] - list_fn_ps_u[3] - list_fn_ps_u[4] - list_fn_ps_u[5] + list_fn_ps_u[6] + list_fn_ps_u[7]
-print ps_y
+print(ps_y)
 
 appl_lower_rng_filter = None
 appl_upper_rng_filter = None
@@ -76,7 +79,7 @@ if (scDist < 0.0531):
     appl_lower_rng_filter = 0
 else:
     appl_lower_rng_filter = ps_y
-print appl_lower_rng_filter
+print(appl_lower_rng_filter)
 
 if (scDist > 40):
     appl_upper_rng_filter = 0
@@ -96,4 +99,5 @@ if (filtered_result == 0):
 else:
     ps_anti_log_y = 10**ps_y
 
-print ps_anti_log_y
+print(ps_anti_log_y)
+print('none: ',None)
